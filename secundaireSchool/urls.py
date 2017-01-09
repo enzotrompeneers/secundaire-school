1be26242ae88e2737a8967cf2fd48fcf4d7470c6
enzotrompeneers <trompeneers@telenet.be>
2017-01-09 12:18:02 +0100

    urls volgorde gewijzigd

--- secundaireSchool/urls.py
+++ secundaireSchool/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^admin/', admin.site.urls),       # /admin (admin page) # EERST
     url(r'^', include('CRUD.urls')),        # / (loads: home, aanbod, wie, contact, intranet: create, read, update, delete)
-    url(r'^admin/', admin.site.urls),       # /admin (admin page)
+    
 ]
