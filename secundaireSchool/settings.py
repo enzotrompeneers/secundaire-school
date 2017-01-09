77be70ef3eb4323f81fa7224a406d3cd06d6818d
enzotrompeneers <trompeneers@telenet.be>
2017-01-09 12:47:39 +0100

    static root gemaakt

--- secundaireSchool/settings.py
+++ secundaireSchool/settings.py
@@ -118,5 +118,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/' 
+# STATIC_ROOT = "/home/myusername/myproject/static"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
